41009303fdbd76e39edd59c0f42a9c5a8c095fc5
Rupert Smith <rupertlssmith@github.com>
2016-10-06 10:06:23 +0100

    Extracted polymer listbox as a stand alone example.

---
+++ Main.elm
@@ -0,0 +1,59 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.App exposing (beginnerProgram)
+import Html.Attributes exposing (title, class, type', attribute)
+import Html.Keyed
+import Platform.Cmd exposing (Cmd)
+
+
+main : Program Never
+main =
+    beginnerProgram { model = init, view = root, update = update }
+
+
+type alias Model =
+    String
+
+
+type Msg
+    = Blah
+
+
+init : Model
+init =
+    "dummy model"
+
+
+update : Msg -> Model -> Model
+update action model =
+    case action of
+        Blah ->
+            model
+
+
+root : Model -> Html Msg
+root model =
+    div [ class "horizontal-section-container" ]
+        [ div []
+            [ h4 [] [ text "Multi-select" ]
+            , div [ class "horizontal-section" ]
+                [ paperListBox [ attribute "multi" "" ]
+                    [ paperItem [] [ text "Bold" ]
+                    , paperItem [] [ text "Italic" ]
+                    , paperItem [] [ text "Underline" ]
+                    , paperItem [] [ text "Strikethrough" ]
+                    ]
+                ]
+            ]
+        ]
+
+
+paperListBox : List (Attribute a) -> List (Html a) -> Html a
+paperListBox =
+    Html.node "paper-listbox"
+
+
+paperItem : List (Attribute a) -> List (Html a) -> Html a
+paperItem =
+    Html.node "paper-item"
---
+++ Readme.md
@@ -0,0 +1,12 @@
+# polymer-elm-listbox
+```sh
+bower install
+elm-make Main.elm --output elm.js --yes
+elm-reactor
+```
+To see the polymer+elm version:
+
+- navigate to http://localhost:8000/index.html
+
+To see the polymer+Html version:
+- navigate to http://localhost:8000/demo.html
